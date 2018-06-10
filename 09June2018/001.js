2606cfb005d5970dc49109a339ef1a48f19a1a57
Susan Nio <susan.wen88@gmail.com>
2018-06-10 13:01:19 +0700

    Refactoring 001

--- 09June2018/001.js
+++ 09June2018/001.js
@@ -1,3 +1,36 @@
+/* Solution 1 - shorter version */
+
+function soal1(param)
+{
+  var print=[];
+  var middle = (param-1)/2;
+  var lowMiddle = Math.floor((param-1)/2);
+  var highMiddle = Math.ceil((param-1)/2);
+  
+  if(param===0){
+    return "invalid input";
+  } 
+  
+  for(var i=0;i<param;i++){
+     if(i===middle || i===lowMiddle || i===highMiddle){
+        print.push("*");
+      } else {
+        print.push(" ");
+      }
+  }
+  return print;
+}
+
+// test case
+console.log( soal1(5) ) //  ['','','*','','']
+console.log( soal1(4)) // ['','*','*','']
+console.log( soal1(7)) // ['','','','*','','','']
+console.log( soal1(10)) // ['','','','','*','*','','','','']
+console.log( soal1(0)) // invalid input 
+console.log( soal1(1)) // ['*']
+
+
+/* Solution 2 -longer version 
 function soal1(param)
 {
   var print=[];
@@ -33,3 +66,5 @@ console.log( soal1(7)) // ['','','','*','','','']
 console.log( soal1(10)) // ['','','','','*','*','','','','']
 console.log( soal1(0)) // invalid input 
 console.log( soal1(1)) // ['*']
+
+*/
